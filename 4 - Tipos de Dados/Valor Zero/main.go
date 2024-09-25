a3ecd5510dc1a8e730263e947a34cc7d51ea5a8d
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2024-09-25 16:47:02 -0300

    Update Valores Zero

--- 4 - Tipos de Dados/Valor Zero/main.go
+++ 4 - Tipos de Dados/Valor Zero/main.go
@@ -8,4 +8,25 @@ func main() {
 
 	var numero int
 	fmt.Println(numero)
+
+	var booleano bool
+	fmt.Println(booleano)
+
+	var ponteiro *int
+	fmt.Println(ponteiro)
+
+	var slice []int
+	fmt.Println(slice)
+
+	var m map[string]int
+	fmt.Println(m)
+
+	var funcao func() string
+	fmt.Println(funcao)
+
+	var inter interface{}
+	fmt.Println(inter)
+
+	var canal chan int
+	fmt.Println(canal)
 }
