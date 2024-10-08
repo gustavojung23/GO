c25abcda80ee1bf25eb042b0cb68de31622f6ec4
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2024-10-08 18:02:58 -0300

    Create Arrays e Slices

---
+++ 10 - Arrays e Slices/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var array1 [5]int
+	array1[0] = 1
+	array1[1] = 2
+	array1[2] = 3
+	array1[3] = 4
+	array1[4] = 5
+	fmt.Println(array1)
+
+	array2 := [2]string{"Posição 1", "Posição 2"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4}
+	fmt.Println(array3)
+
+	slice1 := []int{10, 11, 12, 13}
+	fmt.Println(slice1)
+
+	slice1 = append(slice1, 14)
+	fmt.Println(slice1)
+
+	slice2 := slice1[0:3]
+	fmt.Println(slice2)
+}
