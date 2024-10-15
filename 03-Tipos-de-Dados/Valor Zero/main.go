357b6e0c9c9a155ffeb5b19e6310f67310312226
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2024-10-15 11:31:49 -0300

    Update Valores Zeros

--- 03-Tipos-de-Dados/Valor Zero/main.go
+++ 03-Tipos-de-Dados/Valor Zero/main.go
@@ -3,30 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var texto string
-	fmt.Println(texto)
+	var defaultBool bool
+	var defaultInt int
+	var defaultFloat float64
+	var defaultString string
 
-	var numero int
-	fmt.Println(numero)
-
-	var booleano bool
-	fmt.Println(booleano)
-
-	var ponteiro *int
-	fmt.Println(ponteiro)
-
-	var slice []int
-	fmt.Println(slice)
-
-	var m map[string]int
-	fmt.Println(m)
-
-	var funcao func() string
-	fmt.Println(funcao)
-
-	var inter interface{}
-	fmt.Println(inter)
-
-	var canal chan int
-	fmt.Println(canal)
+	fmt.Println(defaultBool)
+	fmt.Println(defaultInt)
+	fmt.Println(defaultFloat)
+	fmt.Println(defaultString)
 }
