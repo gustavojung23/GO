475cf95c3051f9389c519f1cdaa93689989719db
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2024-10-16 16:59:19 -0300

    Update Função Anonima

--- 04-Funções/Função Anonima/main.go
+++ 04-Funções/Função Anonima/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	func() {
+		fmt.Println("Olá Mundo 1!")
+	}()
+
+	func(texto1 string) {
+		fmt.Println(texto1)
+	}("Olá Mundo 2!")
+
+	retornoTexto := func(texto2 string) string {
+		return fmt.Sprint("Olá ", texto2)
+	}("Mundo 3!")
+
+	fmt.Println(retornoTexto)
 }
